09e636c3a3c1efe6414b81aaaf4087553784bc6e
Sergio Galarza <xderrisk@gmail.com>
2026-03-01 16:49:47 -0500

    Indent

--- extension.js
+++ extension.js
@@ -1,47 +1,60 @@
-import * as Workspace from 'resource:///org/gnome/shell/ui/workspace.js';
-import { Extension, gettext as _ } from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as Workspace from "resource:///org/gnome/shell/ui/workspace.js";
+import {
+	Extension,
+	gettext as _,
+} from "resource:///org/gnome/shell/extensions/extension.js";
 
 let _originalAcceptDrop = null;
 let _settings = null;
 
 export default class MoveInstanceExtension extends Extension {
-    enable() {
-        _settings = this.getSettings();
-        _originalAcceptDrop = Workspace.Workspace.prototype.acceptDrop;
+	enable() {
+		_settings = this.getSettings();
+		_originalAcceptDrop = Workspace.Workspace.prototype.acceptDrop;
 
-        Workspace.Workspace.prototype.acceptDrop = function(source, actor, x, y, time) {
-            let app = source.app || (source.getApp ? source.getApp() : null);
+		Workspace.Workspace.prototype.acceptDrop = function (
+			source,
+			actor,
+			x,
+			y,
+			time,
+		) {
+			let app = source.app || (source.getApp ? source.getApp() : null);
 
-            if (app && _settings) {
-                let windows = app.get_windows();
-                let whitelist = _settings.get_strv('whitelist');
-                
-                let hasMatchedWMClass = windows.some(window => {
-                    let wmClass = window.get_wm_class();
-                    return wmClass && whitelist.some(item => 
-                        wmClass.toLowerCase().includes(item.toLowerCase())
-                    );
-                });
+			if (app && _settings) {
+				let windows = app.get_windows();
+				let whitelist = _settings.get_strv("whitelist");
 
-                let isSingleInstance = !app.can_open_new_window() || hasMatchedWMClass;
+				let hasMatchedWMClass = windows.some((window) => {
+					let wmClass = window.get_wm_class();
+					return (
+						wmClass &&
+						whitelist.some((item) =>
+							wmClass.toLowerCase().includes(item.toLowerCase()),
+						)
+					);
+				});
 
-                if (isSingleInstance && windows.length > 0) {
-                    let windowToMove = windows.find(w => w.get_window_type() === 0) || windows[0];
-                    if (windowToMove) {
-                        windowToMove.change_workspace(this.metaWorkspace);
-                        return true; 
-                    }
-                }
-            }
-            return _originalAcceptDrop.apply(this, arguments);
-        };
-    }
+				let isSingleInstance = !app.can_open_new_window() || hasMatchedWMClass;
 
-    disable() {
-        if (_originalAcceptDrop) {
-            Workspace.Workspace.prototype.acceptDrop = _originalAcceptDrop;
-        }
-        _originalAcceptDrop = null;
-        _settings = null;
-    }
+				if (isSingleInstance && windows.length > 0) {
+					let windowToMove =
+						windows.find((w) => w.get_window_type() === 0) || windows[0];
+					if (windowToMove) {
+						windowToMove.change_workspace(this.metaWorkspace);
+						return true;
+					}
+				}
+			}
+			return _originalAcceptDrop.apply(this, arguments);
+		};
+	}
+
+	disable() {
+		if (_originalAcceptDrop) {
+			Workspace.Workspace.prototype.acceptDrop = _originalAcceptDrop;
+		}
+		_originalAcceptDrop = null;
+		_settings = null;
+	}
 }
--- prefs.js
+++ prefs.js
@@ -1,31 +1,35 @@
-import Adw from 'gi://Adw';
-import { ExtensionPreferences, gettext as _ } from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
+import Adw from "gi://Adw";
+import {
+	ExtensionPreferences,
+	gettext as _,
+} from "resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js";
 
 export default class MoveInstancePrefs extends ExtensionPreferences {
-    fillPreferencesWindow(window) {
-        const settings = this.getSettings();
-        const page = new Adw.PreferencesPage();
-        const group = new Adw.PreferencesGroup({ 
-            title: _("WM_CLASS Configuration"),
-            description: _("Add window class names (e.g., Spotify) to force moving.")
-        });
-        
-        window.add(page);
-        page.add(group);
+	fillPreferencesWindow(window) {
+		const settings = this.getSettings();
+		const page = new Adw.PreferencesPage();
+		const group = new Adw.PreferencesGroup({
+			title: _("WM_CLASS Configuration"),
+			description: _("Add window class names (e.g., Spotify) to force moving."),
+		});
 
-        const row = new Adw.EntryRow({
-            title: _("WM_CLASS List (comma separated)"),
-            text: settings.get_strv('whitelist').join(', ')
-        });
+		window.add(page);
+		page.add(group);
 
-        row.connect('changed', (entry) => {
-            let list = entry.get_text()
-                .split(',')
-                .map(s => s.trim())
-                .filter(s => s !== "");
-            settings.set_strv('whitelist', list);
-        });
+		const row = new Adw.EntryRow({
+			title: _("WM_CLASS List (comma separated)"),
+			text: settings.get_strv("whitelist").join(", "),
+		});
 
-        group.add(row);
-    }
+		row.connect("changed", (entry) => {
+			let list = entry
+				.get_text()
+				.split(",")
+				.map((s) => s.trim())
+				.filter((s) => s !== "");
+			settings.set_strv("whitelist", list);
+		});
+
+		group.add(row);
+	}
 }
