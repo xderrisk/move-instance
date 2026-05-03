dade20d96c51f5efd797f40779258586a338d083
Sergio Galarza <xderrisk@gmail.com>
2026-05-03 13:15:06 -0500

    move state to class instance and use const for variables

--- extension.js
+++ extension.js
@@ -1,23 +1,21 @@
 import * as Workspace from "resource:///org/gnome/shell/ui/workspace.js";
 import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 
-let _originalAcceptDrop = null;
-let _settings = null;
-
 export default class MoveInstanceExtension extends Extension {
   enable() {
-    _settings = this.getSettings();
-    _originalAcceptDrop = Workspace.Workspace.prototype.acceptDrop;
+    this._settings = this.getSettings();
+    this._originalAcceptDrop = Workspace.Workspace.prototype.acceptDrop;
+    const settings = this._settings;
 
     Workspace.Workspace.prototype.acceptDrop = function (source) {
-      let app = source.app || (source.getApp ? source.getApp() : null);
+      const app = source.app || (source.getApp ? source.getApp() : null);
 
-      if (app && _settings) {
-        let windows = app.get_windows();
-        let whitelist = _settings.get_strv("whitelist");
+      if (app) {
+        const windows = app.get_windows();
+        const whitelist = settings.get_strv("whitelist");
 
-        let hasMatchedWMClass = windows.some((window) => {
-          let wmClass = window.get_wm_class();
+        const hasMatchedWMClass = windows.some((window) => {
+          const wmClass = window.get_wm_class();
           return (
             wmClass &&
             whitelist.some((item) =>
@@ -26,10 +24,10 @@ export default class MoveInstanceExtension extends Extension {
           );
         });
 
-        let isSingleInstance = !app.can_open_new_window() || hasMatchedWMClass;
+        const isSingleInstance = !app.can_open_new_window() || hasMatchedWMClass;
 
         if (isSingleInstance && windows.length > 0) {
-          let windowToMove =
+          const windowToMove =
             windows.find((w) => w.get_window_type() === 0) || windows[0];
           if (windowToMove) {
             windowToMove.change_workspace(this.metaWorkspace);
@@ -37,15 +35,16 @@ export default class MoveInstanceExtension extends Extension {
           }
         }
       }
-      return _originalAcceptDrop.apply(this, arguments);
+      return MoveInstanceExtension.prototype._originalAcceptDrop.call(
+        this,
+        source,
+      );
     };
   }
 
   disable() {
-    if (_originalAcceptDrop) {
-      Workspace.Workspace.prototype.acceptDrop = _originalAcceptDrop;
-    }
-    _originalAcceptDrop = null;
-    _settings = null;
+    Workspace.Workspace.prototype.acceptDrop = this._originalAcceptDrop;
+    this._originalAcceptDrop = null;
+    this._settings = null;
   }
 }
